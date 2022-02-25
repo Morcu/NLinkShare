a03aca8c2c2e518e6819cb857faed74f035f3af2
David Morcuende <david.morcuende.c@gmail.com>
2022-02-25 12:49:54 +0100

    Always start on onboarding on new users

--- app/src/main/java/com/example/linkshare/MainActivity.java
+++ app/src/main/java/com/example/linkshare/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.linkshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnLista;
     private Button btnOnboard;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static String NOTION_ID = "NOTION_ID";
+    public static String NOTION_DATABASE_ID = "NOTION_DATABASE_ID";
 
 
     @Override
@@ -20,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
         btnLista = findViewById(R.id.btnLista);
         btnOnboard = findViewById(R.id.btnOnboard);
+
+        String notion_id = loadData(NOTION_ID);
+        String notion_database_id = loadData(NOTION_DATABASE_ID);
+
+        Log.e("ID", notion_id);
+        Log.e("ID", notion_database_id);
+
+        if (notion_id.isEmpty() || notion_database_id.isEmpty()){
+            Intent myIntent = new Intent(this, OnboardActivity.class);
+            startActivity(myIntent);
+        }
 
         btnLista.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +53,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+    }
+
+    public String loadData(String key) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        return sharedPreferences.getString(key, "");
     }
 }
